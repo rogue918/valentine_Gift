36ec6409f44e616f9cbafbbadbe2b5f389702d42
rogue918 <atmajeet06@gmail.com>
2026-02-08 11:22:52 +0530

    Update Valentine.jsx

--- src/components/Valentine.jsx
+++ src/components/Valentine.jsx
@@ -5,7 +5,7 @@ const noMessages = [
     "Oh? 👀 You clicked No already?",
     "Wow. Bold choice. Very bold.",
     "Are you *sure* sure? Because my heart disagrees.",
-    "Harsitaa… that button is starting to look suspicious 😏",
+    "Sanikaaaa… that button is starting to look suspicious 😏",
     "Okay but imagine saying No to *this face* 🥺",
     "This is getting awkward. Just say Yes already.",
     "At this point the No button is just decoration 😂",
